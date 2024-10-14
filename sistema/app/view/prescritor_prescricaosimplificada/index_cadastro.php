afc131d088970840d9a8db57b9b4437c5e940ebf
João Victor Lima <victorespucoc@gmail.com>
2024-10-14 08:44:07 -0300

    prescricao simplificada

--- sistema/app/view/prescritor_prescricaosimplificada/index_cadastro.php
+++ sistema/app/view/prescritor_prescricaosimplificada/index_cadastro.php
@@ -31,16 +31,16 @@
                             );
                     echo $item_dados;
                 ?>
-                <div class="row">
-                    <div class="text-center">
-                        <button type="button" onclick="rangeCaloria(2400);" class="btn <?php echo ($tab1=="cadastrar"?"btn-warning":"btn-secondary"); ?> btn-sm" id="cadastrar_paciente">
-                            <span class="btn-icon-right"><i class="fa fa-user"></i></span>
-                            CADASTRAR PACIENTE
+                <div class="row form-group">
+                    <div class="text-center col-sm-12">
+                        <button type="button" class="btn btn-secundary btn-sm">
+                            <span class="btn-icon-right"><i class="fa-regular fa-pen-to-square"></i></span>
+                            Inserir valores manualmente
                         </button>
                         &nbsp;
-                        <button type="button" class="btn <?php echo ($tab1=="buscar"?"btn-warning":"btn-secondary"); ?> btn-sm" id="buscar_paciente">
-                            <span class="btn-icon-right"><i class="fa fa-search"></i></span>
-                            BUSCAR PACIENTE CADASTRADO
+                        <button type="button" class="btn btn-warning btn-sm" id="buscar_paciente">
+                            <span class="btn-icon-right"><i class="fa-solid fa-calculator"></i></span>
+                            Desejo realizar os cálculos
                         </button>
                     </div>
                 </div>
