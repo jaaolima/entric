f53ab36e158c859a34c40df080322b62dcf914f2
João Victor Lima <victorespucoc@gmail.com>
2024-10-17 10:04:43 -0300

    prescricao simplificada

--- sistema/app/view/prescritor_prescricaosimplificada/index_cadastro.php
+++ sistema/app/view/prescritor_prescricaosimplificada/index_cadastro.php
@@ -90,5 +90,190 @@
                 
             </form>
         </div>
+        <div id="div_buscar_paciente" style="<?php echo ($tab1=="buscar"?"display: block;":"display: none;"); ?>">
+            <div id="listar_pacientes" style="display: none;">
+                <form action="prescritor_relatorioalta" id="form_atualizar_paciente" method="post" autocomplete="off" onsubmit="return false">
+                    <input type="hidden" name="action" id="action" value="atualizar"/>
+                    <input type="hidden" name="up_id" id="up_id" value=""/>
+                    <?php
+                    $item_dados =  $html->addRow(
+                                    array(
+                                        "up_nome" => array(
+                                            "col" => 12,
+                                            "label" => "Nome do Paciente:",
+                                            "required" => "required",
+                                        ),
+                                        "up_celular" => array(
+                                            "col" => 4,
+                                            "label" => "Celular:",
+                                            "class" => "telefone",
+                                            "placeholder" => "(99) 99999-9999",
+                                            "required" => "required"
+                                        ),
+                                        "up_pertence" => array(
+                                            "col" => 4,
+                                            "label" => "De quem é o celular?",
+                                            "required" => "required",
+                                            "select" => array(
+                                                "" => "selecione...",
+                                                "paciente" => "Paciente",
+                                                "familiar" => "Familiar",
+                                                "cuidador" => "Cuidador",
+                                            )
+                                        ),
+                                        "up_parentesco" => array(
+                                            "col" => 4,
+                                            "label" => "Qual o Parentesco?"
+                                        ),
+                                        "up_data_nascimento" => array(
+                                            "col" => 4,
+                                            "label" => "Data de Nascimento:",
+                                            "class" => "data",
+                                            "placeholder" => "dd/mm/aaaa",
+                                            "required" => "required"
+                                        ),
+                                        "up_sexo" => array(
+                                            "col" => 4,
+                                            "label" => "Sexo:",
+                                            "radiobutton" => array(
+                                                "masculino" => "Masculino",
+                                                "feminino" => "Feminino",
+                                            ),
+                                            "required" => "required"
+                                        ),
+                                        "up_email" => array(
+                                            "col" => 4,
+                                            "label" => "E-mail:",
+                                            "required" => "required"
+                                        ),
+                                        "up_cpf" => array(
+                                            "col" => 3,
+                                            "label" => "CPF:",
+                                            "class" => "cpf",
+                                            "placeholder" => "999.999.999-99",
+                                            "readonly" => "readonly"
+                                        ),
+                                        "up_cpf_possui" => array(
+                                            "col" => 3,
+                                            "label" => "Desconhece / Não possui",
+                                            "checkbox" => true
+                                        ),
+                                        "up_mae" => array(
+                                            "col" => 4,
+                                            "label" => "Nome da Mãe:",
+                                            "required" => "required"
+                                        ),
+                                        "up_mae_possui" => array(
+                                            "col" => 2,
+                                            "label" => "Desconhece",
+                                            "checkbox" => true
+                                        )
+                                    )
+                                );
+                        echo $item_dados;
+                    ?>
+                    <div class="form-group row pt-5">
+                        <div class="col-sm-6 text-right">
+                            <button type="button" onclick="fc_limpar_paciente();" class="btn btn-secondary btn-form">VOLTAR</button>
+                        </div>
+                        <div class="col-sm-6 text-left">
+                            <button type="submit" onclick="fc_atualizar_paciente();" class="btn btn-warning btn-form">Atualizar</button>
+                        </div>
+                    </div>
+                </form>
+
+                <div class="table-responsive pt-5" style="max-height: 400px;">
+                    <table class="table table-bordered table-hover table-striped" id="table_lista_pacientes">
+                        <thead>
+                            <tr>
+                                <th scope="col">Relatório</th>
+                                <th scope="col">Data</th>
+                                <th scope="col" class="text-center">Visualização Disponível para o Paciente</th>
+                                <th scope="col">Editar/Desativar</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="form-group row pt-5">
+                    <div class="col-sm-6 text-left">
+                        <button type="button" onclick="window.location.href='prescritor_relatorioalta'" class="btn btn-secondary btn-form">VOLTAR</button>
+                    </div>
+                    <div class="col-sm-6 text-right">
+                        <button type="button" class="btn btn-warning btn-form" id="gerar_relatorio">GERAR NOVO RELATÓRIO</button>
+                    </div>
+                </div>
+            </div>
+
+            <div id="buscar_pacientes" class="display: block;">
+                <form action="prescritor_relatorioalta" id="prescritor_relatorioalta" name="prescritor_relatorioalta" method="post" onsubmit="return false">
+                    <input type="hidden" name="action" id="action" value="buscar"/>
+                    <input type="hidden" name="id_paciente" id="id_paciente" value="" />
+                    <input type="hidden" name="id_relatorio" id="id_relatorio" value="" />
+                    <input type="hidden" name="relatorio_code" id="relatorio_code" value="" />
+
+                    <?php
+                    $item_dados =  $html->addRow(
+                                    array(
+                                        "nome" => array(
+                                            "col" => 12,
+                                            "label" => "Nome do Paciente:"
+                                        ),
+                                        "data_nascimento" => array(
+                                            "col" => 6,
+                                            "label" => "Data de Nascimento:",
+                                            "class" => "data",
+                                            "placeholder" => "dd/mm/aaaa"
+                                        ),
+                                        "cpf" => array(
+                                            "col" => 6,
+                                            "label" => "CPF:",
+                                            "class" => "cpf",
+                                            "placeholder" => "999.999.999-99"
+                                        )
+                                    )
+                                );
+                        echo $item_dados;
+                    ?>
+
+                    <div class="modal fade" id="modal_retorno_pacientes" tabindex="-1" role="dialog" aria-labelledby="modal_retorno_pacientes" aria-hidden="true">
+                        <div class="modal-dialog modal-lg">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title">Pacientes Encontrados</h5>
+                                    <button type="button" class="close" data-dismiss="modal"><span>&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="table-responsive">
+                                        <table class="table table-bordered table-hover table-clique" id="table_retorno_pacientes">
+                                            <thead>
+                                                <tr>
+                                                    <th scope="col">Nome</th>
+                                                    <th scope="col">CPF</th>
+                                                    <th scope="col">Mãe</th>
+                                                    <th scope="col">Data de Nascimento</th>
+                                                    <th scope="col">Sexo</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="form-group row pt-5">
+                        <div class="col-sm-12 text-center">
+                            <button type="submit" onclick="fc_buscar_paciente();" class="btn btn-secondary btn-form">BUSCAR</button>                                                             
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
 </div>
