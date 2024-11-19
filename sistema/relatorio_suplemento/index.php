fea63658802fa0991f4e64d78888497a6ea0afb2
João Victor Lima <victorespucoc@gmail.com>
2024-11-19 15:47:15 -0300

    relatorio

--- sistema/relatorio_suplemento/index.php
+++ sistema/relatorio_suplemento/index.php
@@ -184,12 +184,12 @@ if (trim($relatorio['preparo'])=="") $relatorio['preparo'] = $config['preparo'];
 			<?php } ?>
 
 			<p class="text-left subtitutlo"><img src="imagem/simbolo.png" width="18px" border="0" style="vertical-align:bottom; margin-right: 5px;" /> O QUE É A TERAPIA NUTRICIONAL POR VIA ORAL?</p>
-			<div style="display:flex;">
+			<div style="display:flex;margin-top:15px;">
 				<div style="width:68%;">
 					<p>A Terapia Nutricional Enteral po Via Oral, também conhecida como <span style="color:#0092c5;">suplemento nutricional</span>, completa as calorias, proteínas e nutrientes que não estão sendo supridos com a dieta convencional, e tem como objetivo a <span style="color:#0092c5;">recuperação ou manutenção da saúde e do estado nutricional</span>.</p>
 				</div>
 				<div style="text-align:center;width:32%;">
-					<h4 style="color:#45cfb3;">SAIBA MAIS!</h4>
+					<h4 style="color:#45cfb3;margin:0px;">SAIBA MAIS!</h4>
 					<img src="imagem/qrcode.png" style="display:inline-block;" width="120" alt="">
 				</div>
 			</div>
@@ -200,6 +200,7 @@ if (trim($relatorio['preparo'])=="") $relatorio['preparo'] = $config['preparo'];
 
 		<?php 
 		if (((!$p_produtos) and (!$p_footer)) or ($p_header)){
+			var_dump($relatorio['fracionamento_dia']);
 		?>
 			<p class="text-left subtitutlo"><img src="imagem/simbolo.png" width="18px" border="0" style="vertical-align:bottom; margin-right: 5px;" /> CONDUTA</p>
 			<p>Utilizar <?php echo $relatorio['fracionamento_dia']; ?> vezes ao dia por <?php echo $relatorio['qto_tempo']; ?>.</p>
