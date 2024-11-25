c141ec33e860e0c3794f3bfee5a8e489f361a795
João Victor Lima <victorespucoc@gmail.com>
2024-11-25 15:57:46 -0300

    relatorio

--- sistema/api/src/routes/api.php
+++ sistema/api/src/routes/api.php
@@ -2282,7 +2282,12 @@ $app->group("", function () use ($app) {
 		                                    $kcal_valor_maximo = floor($kcal_valor_maximo);
 		                                    if ($kcal_valor_minimo <= $kcal_valor_maximo){
 		                                        $qtd_bolsas = $kcal_valor_minimo;
-		                                        $_kcal_total = $qtd_bolsas * $_volume * $_medida_dc;
+												$kcal = 1;
+		                                        if (trim($produtos[$i]['kcal']) <> ""){
+		                                            $kcal = trim($produtos[$i]['kcal']);
+		                                            $kcal = str_replace(",",".", $kcal);
+		                                        }
+		                                        $_kcal_total = $qtd_bolsas * $_volume * $kcal;
 		                                    
 
 		                                        /*
