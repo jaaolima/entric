97ef1798c4207ab38a6e7c58b765d2abfbd23dde
João Victor Lima <victorespucoc@gmail.com>
2024-11-25 15:44:49 -0300

    relatorio

--- sistema/relatorio_suplemento/imprimir/index.php
+++ sistema/relatorio_suplemento/imprimir/index.php
@@ -5,9 +5,9 @@ set_time_limit(0);
 
 function Redirect($url, $permanent = false){
     if (headers_sent() === false){
-        header('Location: https://sistema.entric.com.br/', true, ($permanent === true) ? 301 : 302);
+        header('Location: https://entric.com.br/', true, ($permanent === true) ? 301 : 302);
     }else{
-        echo '<script type="text/javascript">window.location.href = "https://sistema.entric.com.br/";</script>';
+        echo '<script type="text/javascript">window.location.href = "https://entric.com.br/";</script>';
     }
     exit();
 }
@@ -24,7 +24,7 @@ $url = $_GET['url'];
         padding: 0px;
     }
 </style>
-<iframe src="https://pdf.entric.com.br/<?php echo $url;?>" id="relatorio" style="margin: 0px; padding: 0px; border: 0px; width: 100%; height: 100%;"></iframe>
+<iframe src="https://entric.com.br/<?php echo $url;?>" id="relatorio" style="margin: 0px; padding: 0px; border: 0px; width: 100%; height: 100%;"></iframe>
 <?php
 /*
 <script>
