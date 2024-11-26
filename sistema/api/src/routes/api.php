ab0a786ec57e3cd5ba843a08efd90279e213def5
João Victor Lima <victorespucoc@gmail.com>
2024-11-26 10:01:14 -0300

    erro fecahdo

--- sistema/api/src/routes/api.php
+++ sistema/api/src/routes/api.php
@@ -2317,6 +2317,7 @@ $app->group("", function () use ($app) {
 		                                        $valor_proteico = $_ptn_total;
 		                                        $calorias_dia = $_kcal_total;
 		                                        $proteina_dia = $_ptn_total;
+												var_dump(((intval($margem_calorica[0]) <= intval($_kcal_total)) and (intval($margem_calorica[1]) >= intval($_kcal_total))));
 		                                        if (
 		                                            ((intval($margem_calorica[0]) <= intval($_kcal_total)) and (intval($margem_calorica[1]) >= intval($_kcal_total))) and
 		                                            ((intval($margem_proteica[0]) <= intval($_ptn_total)) and (intval($margem_proteica[1]) >= intval($_ptn_total)))
