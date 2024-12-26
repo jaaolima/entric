40e3fad9097eaa99d68979453f5d6bcf25ca0e51
João Victor Lima <victorespucoc@gmail.com>
2024-12-26 10:42:22 -0300

    Login com usuário do ibranutro

--- sistema/api/src/autoload.php
+++ sistema/api/src/autoload.php
@@ -2,6 +2,7 @@
 require "config/config.php";
 require "libs/auth.php";
 require "libs/connection.php";
+require "libs/connection_ibranutro.php";
 require "libs/common.php";
 require "routes/api.php";
 ?>
