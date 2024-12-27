a0d5fcc0fb1190e8fc1e51c2a41598b636fb8863
João Victor Lima <victorespucoc@gmail.com>
2024-12-27 10:10:15 -0300

    Login com usuário do ibranutro

--- sistema/app/libs/bootstrap.php
+++ sistema/app/libs/bootstrap.php
@@ -4,7 +4,7 @@ date_default_timezone_set('America/Sao_Paulo');
 
 require_once (ROOT . DS . 'app'. DS . 'config' . DS . 'conf6ion.php');
 require_once (ROOT . DS . 'app'. DS . 'libs' . DS . 'common.php');
-if ((!startsWith($url,"login")) and (!startsWith($url,"logout")) and (!startsWith($url,"v/")) and (!startsWith($url,"senha")) and (!startsWith($url,"cadastro/"))  and (!startsWith($url,"paciente_videosalta"))  and (!startsWith($url,"paciente_distribuidores")) ) {    
+if ((!startsWith($url,"login")) and (!startsWith($url,"logout")) and (!startsWith($url,"v/")) and (!startsWith($url,"senha")) and (!startsWith($url,"cadastro/"))  and (!startsWith($url,"paciente_videosalta"))  and (!startsWith($url,"paciente_distribuidores")) and (!startsWith($url,"login/loginDireto"))) {    
     checkLoginSession();
 }
 
