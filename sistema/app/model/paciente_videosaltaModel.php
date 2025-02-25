48eafea32e8ac0a5e78007db512c2d36bfb52c1c
João Victor Lima <victorespucoc@gmail.com>
2025-02-25 10:16:34 -0300

    relatorio simplifificada

--- sistema/app/model/paciente_videosaltaModel.php
+++ sistema/app/model/paciente_videosaltaModel.php
@@ -4,7 +4,7 @@ class Paciente_videosaltaModel extends Model {
 
     function getDados() {
         global $bruker;
-        $retorno = httpPostAuth("paciente_videosalta_getDados");
+        $retorno = httpPostAuth("paciente_videosalta_getDados", array(   "token" => $_SESSION['token']));
         return $retorno;
     }
     
