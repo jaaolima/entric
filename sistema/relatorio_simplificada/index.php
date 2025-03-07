5ff0ee75d61c9f773eec2c9bfb3381729a5ae0c7
João Victor Lima <victorespucoc@gmail.com>
2025-03-07 11:12:33 -0300

    relatorio alterações

--- sistema/relatorio_simplificada/index.php
+++ sistema/relatorio_simplificada/index.php
@@ -148,7 +148,7 @@ if (trim($relatorio['preparo'])=="") $relatorio['preparo'] = $config['preparo'];
 
 				.page .background:first-child {
 					left: 1cm !important;
-					top: -2cm !important;
+					top: -1.2cm !important;
 					width: 150px !important;
 				}
 
