ca3215ac75b624677cb7d7fc5b613e47eae40cec
João Victor Lima <victorespucoc@gmail.com>
2025-03-13 10:52:45 -0300

    login entric

--- sistema/app/model/utilModel.php
+++ sistema/app/model/utilModel.php
@@ -3,29 +3,22 @@ class UtilModel extends Model {
 
     public function stlog() {
         global $bruker;
-
-        $qdata = array(':id_usuario'=> $bruker->usuario['id_usuario'], 
+        
+        if($_SESSION['login'] == 'entric'){
+            $qdata = array(':id_usuario'=> $bruker->usuario['id'], 
+            ':login'=> $bruker->usuario['email'], 
+            ':funcao'=> $_SERVER['QUERY_STRING'], 
+            ':ipaddress'=> get_ip_address(), 
+            ':dados'=> json_encode($_POST).'|||'.json_encode($_POST), 
+            ':data_criacao'=> date('Y-m-d H:i:s'));
+        }else{
+            $qdata = array(':id_usuario'=> $bruker->usuario['id_usuario'], 
             ':login'=> $bruker->usuario['ds_usuario'], 
             ':funcao'=> $_SERVER['QUERY_STRING'], 
             ':ipaddress'=> get_ip_address(), 
             ':dados'=> json_encode($_POST).'|||'.json_encode($_POST), 
             ':data_criacao'=> date('Y-m-d H:i:s'));
-        
-        // if($_SESSION['login'] == 'ibranutro'){
-        //     $qdata = array(':id_usuario'=> $bruker->usuario['id_usuario'], 
-        //     ':login'=> $bruker->usuario['ds_usuario'], 
-        //     ':funcao'=> $_SERVER['QUERY_STRING'], 
-        //     ':ipaddress'=> get_ip_address(), 
-        //     ':dados'=> json_encode($_POST).'|||'.json_encode($_POST), 
-        //     ':data_criacao'=> date('Y-m-d H:i:s'));
-        // }elseif($_SESSION['login'] == 'entric'){
-        //     $qdata = array(':id_usuario'=> $bruker->usuario['id'], 
-        //     ':login'=> $bruker->usuario['email'], 
-        //     ':funcao'=> $_SERVER['QUERY_STRING'], 
-        //     ':ipaddress'=> get_ip_address(), 
-        //     ':dados'=> json_encode($_POST).'|||'.json_encode($_POST), 
-        //     ':data_criacao'=> date('Y-m-d H:i:s'));
-        // }
+        }
 
         $retorno = httpPostAuth("util_stlog", array("token" => $_SESSION['token'],
                                                     "qdata" => $qdata ));       
