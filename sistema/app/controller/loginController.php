aa5f7a8f490a10ee52d655b8d30b79c8d96591f4
João Victor Lima <victorespucoc@gmail.com>
2025-03-18 10:27:18 -0300

    cadastro usuario entric

--- sistema/app/controller/loginController.php
+++ sistema/app/controller/loginController.php
@@ -144,6 +144,7 @@ class LoginController extends Controller {
 
                     }else{
                         if ($logar == "email"){
+                            var_dump("chegou");
                             alertretorno("toastr['error']('Esta conta ainda não está ativa.', '', {positionClass: 'toast-top-right' });");
 
                         }else{
