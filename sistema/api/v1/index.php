864b229fdd40932e8264288646d4c9a4ea95a2d6
João Victor Lima <victorespucoc@gmail.com>
2025-03-20 10:32:48 -0300

    enviar email

--- sistema/api/v1/index.php
+++ sistema/api/v1/index.php
@@ -3,7 +3,6 @@ error_reporting(E_ALL);
 ini_set('display_errors','On');
 date_default_timezone_set('America/Sao_Paulo');
 
-require '../vendor/autoload.php';
 
 $configuration = [
     'settings' => [
@@ -16,6 +15,7 @@ $c = new \Slim\Container($configuration);
 $app = new \Slim\App($c);
 
 require '../src/autoload.php';
+require '../vendor/autoload.php';
 
 $app->run();
 ?>
