2b3227580a99bc089960458bd05cd7f3279f654d
João Victor Lima <victorespucoc@gmail.com>
2025-03-24 14:32:21 -0300

    enviar email

--- sistema/api/src/routes/api.php
+++ sistema/api/src/routes/api.php
@@ -2206,7 +2206,7 @@ $app->group("", function () use ($app) {
 		                        $grama = json_decode($produtos[$i]['medida_g'], true);
 
 		                        $titulo = '<td rel="'.$produtos[$i]['id'].'" rowspan="'.count($medida_dc).'"><div class="form-check col-sm-12"><input id="check_dieta'.$produtos[$i]['id'].'" rel="'.$produtos[$i]['id'].'" class="form-check-input styled-checkbox check_dieta" onclick="check_dieta(this, '.$produtos[$i]['id'].');" name="check_dieta'.$produtos[$i]['id'].'" type="checkbox" value=""><label for="check_dieta'.$produtos[$i]['id'].'" class="form-check-label collapseSistema check-green">&nbsp;</label></div> </td>';
-		                        $titulo .= '<td rel="'.$produtos[$i]['id'].'" rowspan="'.count($medida_dc).'">'.$produtos[$i]['nome']." </td>';
+		                        $titulo .= '<td rel="'.$produtos[$i]['id'].'" rowspan="'.count($medida_dc).'">'.$produtos[$i]['nome'].'" </td>';
 
 		                        $cont_array = 0;
 		                        $rowspan = 0;
