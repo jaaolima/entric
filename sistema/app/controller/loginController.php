ec127de15f7390f22b07d9ede45b4e55d01b7d98
João Victor Lima <victorespucoc@gmail.com>
2025-03-25 14:28:10 -0300

    enviar email recuperar senha

--- sistema/app/controller/loginController.php
+++ sistema/app/controller/loginController.php
@@ -157,7 +157,7 @@ class LoginController extends Controller {
             else if ($_POST['_ac'] == "nova_prescritor"){
                 $senha = new SenhaModel();
                 $checar_codigo = $senha->checarCodigoSenhaPrescritor($_POST['_cd']);
-                var_dump($checar_codigo);
+                // var_dump($checar_codigo);
                 if ($checar_codigo){
                     if ($_POST["nova_senha"] == $_POST["confirmar_nova_senha"]){
                         $atualizar = $this->LoginModel->atualizarSenhaPrescritor($_POST); 
