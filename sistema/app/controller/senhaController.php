18094a69c72ce4111599e158647a77d96f69a4a0
João Victor Lima <victorespucoc@gmail.com>
2025-03-25 08:38:07 -0300

    enviar email recuperar senha

--- sistema/app/controller/senhaController.php
+++ sistema/app/controller/senhaController.php
@@ -175,7 +175,7 @@ class SenhaController extends Controller {
 
     }
 
-    function nova_prescritor($codigo = null) {
+    function nova_prescritor($codigo = 'STV6UDNrY2EveCtrb212QXI1UnQrWks5T1ZXM0ZZYzlHSThydkNCR0JuTT0=') {
         var_dump($codigo);
 
         if ($codigo){
