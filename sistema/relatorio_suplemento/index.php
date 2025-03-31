1e8ebb4fe0ac7a5c772f06a535c4b5e3df80a47e
João Victor Lima <victorespucoc@gmail.com>
2025-03-31 15:54:14 -0300

    listar pacientes ibranutro

--- sistema/relatorio_suplemento/index.php
+++ sistema/relatorio_suplemento/index.php
@@ -789,7 +789,7 @@ if (trim($relatorio['preparo'])=="") $relatorio['preparo'] = $config['preparo'];
 				<div style="justify-content:center;text-align: center;display: flex;margin-top: 50px;">
 					<div style="width: 250px;border-top: 1px solid;">
 						<div style="margin-bottom:15px;">
-							<p>Precritor</p>
+							<p>Prescritor</p>
 							<p style="margin:0px;">(Assinatura e Carimbo)</p>
 						</div>
 						<?php if($nome_hospital != '') : ?>
