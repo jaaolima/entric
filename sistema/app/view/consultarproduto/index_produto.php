db2ba84b4cf315f4ac97a16a8f287b5a9c720ebb
João Victor Lima <victorespucoc@gmail.com>
2025-04-01 10:35:36 -0300

    prescritor modulo

--- sistema/app/view/consultarproduto/index_produto.php
+++ sistema/app/view/consultarproduto/index_produto.php
@@ -194,37 +194,37 @@
                                 <div class="row">
                                     <div class="col-6 p-4">
                                         <div class="form-check col-sm-12">
-                                            <input id="categoria_modulo_proteina" required name="cat_modulo[]" class="form-check-input radio-outlined" type="radio" value="Proteína">
+                                            <input id="categoria_modulo_proteina" required name="cat_modulo[]" class="form-check-input styled-checkbox" type="checkbox" value="Proteína">
                                             <label for="categoria_modulo_proteina" class="form-check-label radio-green">Proteína</label>
                                         </div>
                                         <div class="form-check col-sm-12">
-                                            <input id="categoria_modulo_colageno_aminoacidos" name="cat_modulo[]" class="form-check-input radio-outlined" type="radio" value="Colágeno ou Aminoácidos">
+                                            <input id="categoria_modulo_colageno_aminoacidos" name="cat_modulo[]" class="form-check-input styled-checkbox" type="checkbox" value="Colágeno ou Aminoácidos">
                                             <label for="categoria_modulo_colageno_aminoacidos" class="form-check-label radio-green">Colágeno ou Aminoácidos</label>
                                         </div>
                                         <div class="form-check col-sm-12">
-                                            <input id="categoria_modulo_carboidrato" name="cat_modulo[]" class="form-check-input radio-outlined" type="radio" value="Carboidrato">
+                                            <input id="categoria_modulo_carboidrato" name="cat_modulo[]" class="form-check-input styled-checkbox" type="checkbox" value="Carboidrato">
                                             <label for="categoria_modulo_carboidrato" class="form-check-label radio-green">Carboidrato</label>
                                         </div>
                                         <div class="form-check col-sm-12">
-                                            <input id="categoria_modulo_lipideo" name="cat_modulo[]" class="form-check-input radio-outlined" type="radio" value="Lipídeo">
+                                            <input id="categoria_modulo_lipideo" name="cat_modulo[]" class="form-check-input styled-checkbox" type="checkbox" value="Lipídeo">
                                             <label for="categoria_modulo_lipideo" class="form-check-label radio-green">Lipídeo</label>
                                         </div>
                                     </div>
                                     <div class="col-6 p-4">
                                         <div class="form-check col-sm-12">
-                                            <input id="categoria_modulo_fibras" name="cat_modulo[]" class="form-check-input radio-outlined" type="radio" value="Fibras">
+                                            <input id="categoria_modulo_fibras" name="cat_modulo[]" class="form-check-input styled-checkbox" type="checkbox" value="Fibras">
                                             <label for="categoria_modulo_fibras" class="form-check-label radio-green">Fibras</label>
                                         </div>
                                         <div class="form-check col-sm-12">
-                                            <input id="categoria_modulo_probioticos" name="cat_modulo[]" class="form-check-input radio-outlined" type="radio" value="Probióticos">
+                                            <input id="categoria_modulo_probioticos" name="cat_modulo[]" class="form-check-input styled-checkbox" type="checkbox" value="Probióticos">
                                             <label for="categoria_modulo_probioticos" class="form-check-label radio-green">Probióticos</label>
                                         </div>
                                         <div class="form-check col-sm-12">
-                                            <input id="categoria_modulo_simbioticos" name="cat_modulo[]" class="form-check-input radio-outlined" type="radio" value="Simbióticos">
+                                            <input id="categoria_modulo_simbioticos" name="cat_modulo[]" class="form-check-input styled-checkbox" type="checkbox" value="Simbióticos">
                                             <label for="categoria_modulo_simbioticos" class="form-check-label radio-green">Simbióticos</label>
                                         </div>
                                         <div class="form-check col-sm-12">
-                                            <input id="categoria_modulo_espessante" name="cat_modulo[]" class="form-check-input radio-outlined" type="radio" value="Espessante">
+                                            <input id="categoria_modulo_espessante" name="cat_modulo[]" class="form-check-input styled-checkbox" type="checkbox" value="Espessante">
                                             <label for="categoria_modulo_espessante" class="form-check-label radio-green">Espessante</label>
                                         </div>
                                     </div>
