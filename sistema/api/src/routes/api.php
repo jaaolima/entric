181fbc5a9e723b0fc13561e9cbfca6485888c65e
João Victor Lima <victorespucoc@gmail.com>
2025-04-15 11:48:00 -0300

    modulos

--- sistema/api/src/routes/api.php
+++ sistema/api/src/routes/api.php
@@ -5983,7 +5983,7 @@ $app->group("", function () use ($app) {
 			if ($usuario){
 		        $relatorios = $db->select_to_array("relatorios_suplemento r
 													inner join pacientes_suplemento p on r.id_paciente = p.id",
-		                                            "r.*, 'Suplemento' as tipo, p.nome, DATE_FORMAT(p.data_nascimento, '%d/%m/%Y') as data_nascimento, DATE_FORMAT(p.data_criacao, '%d/%m/%Y h:i') as data_criacao",
+		                                            "r.*, 'Suplemento' as tipo, p.nome, DATE_FORMAT(p.data_nascimento, '%d/%m/%Y') as data_nascimento, DATE_FORMAT(p.data_criacao, '%d/%m/%Y %h:%i') as data_criacao",
 		                                            "where p.id_prescritor_ibranutro = :id
 		                                            ORDER BY r.id ASC", 
 		                                            [':id' => $usuario['id_usuario']]);
