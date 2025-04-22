9d3b4f318eb1f68a036b3301137df90d26c033d7
João Victor Lima <victorespucoc@gmail.com>
2025-04-22 10:28:03 -0300

    alterar relatorio

--- sistema/app/view/prescritor_relatorioalta/js.php
+++ sistema/app/view/prescritor_relatorioalta/js.php
@@ -347,7 +347,12 @@ function fc_editar_relatorio(id_relatorio){
                  dobras_lado = JSON.parse(relatorio.dobras_lado);
                  $("input[name='dobras_lado[0]'][value='"+dobras_lado+"']").attr("checked","checked");
 
-
+                 $("input[class='calorias_total']").val(nec_calorias_peso_valor);
+                 necessidades_calorias_total(null, null);
+                 $("input[class='proteinas_total']").val(nec_proteinas_peso_valor);
+                 necessidades_proteinas_total(null, null);
+                 $("input[class='agua_total']").val(nec_agua_peso_valor);
+                 necessidades_agua_total(null, null);
 
             }
             else if (data.error){
