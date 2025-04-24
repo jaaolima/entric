d22434069334279872d079b40903ddc07cfe1800
João Victor Lima <victorespucoc@gmail.com>
2025-04-24 09:03:38 -0300

    cadastro fabricante

--- sistema/app/view/consultarproduto/js.php
+++ sistema/app/view/consultarproduto/js.php
@@ -753,8 +753,8 @@ function modalFabricantes(){
             var _table = $("#modal_fabricantes").find(".entric_table");
             _table.find("tbody").html("");
             jQuery.each(dados.rm, function(i,data) {
-                console.log(i);
-                if(i == 1){
+                console.log(i, data);
+                if(i == 'Abbott'){
                     _table.append("<tr><td>" + data + "</td><td class='text-center'><a href='javascript:modalFabricantesEditar(\""+ data +"\");' class='btn-sm text-info'><i class='fa fa-pencil' aria-hidden='true'></i></a> <a href='javascript:modalFabricantesDelete(\""+ data +"\");' class='btn-sm text-danger'><i class='fa fa-trash-o' aria-hidden='true'></i></a></td></tr>");
                 }
             });
