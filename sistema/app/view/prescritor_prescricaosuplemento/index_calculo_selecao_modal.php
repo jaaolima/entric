802341feea8c65f323f176d5259e4428092eec89
João Victor Lima <victorespucoc@gmail.com>
2025-05-20 09:40:12 -0300

    produto especializado

--- sistema/app/view/prescritor_prescricaosuplemento/index_calculo_selecao_modal.php
+++ sistema/app/view/prescritor_prescricaosuplemento/index_calculo_selecao_modal.php
@@ -2,10 +2,14 @@
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="modal_selecaoTitle">Seleção da dieta</h5>
+                <h5 class="modal-title" id="modal_selecaoTitle" style="width:100%;text-align:center;">Seleção da dieta</h5>
                 <button type="button" class="close" data-dismiss="modal"><span>&times;</span></button>
             </div>
             <div class="modal-body" id="modal-pdf-body">
+                <div class="form-check col-sm-4">
+                    <input id="calculo_produto_especializado" name="produto_especializado" class="form-check-input styled-checkbox" type="checkbox" value="S">
+                    <label for="calculo_produto_especializado" class="form-check-label check-green"> <img src="../../../public/assets/images/bandeira.png" alt=""> Produto Especializado</label>
+                </div>
                 <form id="modal_form_selecao" action="prescritor_relatorioalta" method="post" autocomplete="off" onsubmit="return false">
 
                     <table class="table table-bordered" id="dietas_dc">
