af8c67a6394236673cfaeab5a6e56bb38b0c847f
João Victor Lima <victorespucoc@gmail.com>
2025-06-17 09:00:29 -0300

    não deixar cadastrar paciente en duas vezes

--- sistema/app/model/loginModel.php
+++ sistema/app/model/loginModel.php
@@ -164,7 +164,7 @@ class LoginModel extends Model {
                                     "login" => $_SESSION['login'],
                                     "id_paciente" => $id_paciente,
                                     "sistema" => $sistema));
-                                $_SESSION['paciente_redirect'] = ['sistema' => null,'id_paciente' => $id_paciente, 'buscar' => $buscar, 'ds_nome' => $paciente['nome']];
+                                $_SESSION['paciente_redirect'] = ['sistema' => $sistema,'id_paciente' => $id_paciente, 'buscar' => $buscar, 'ds_nome' => $paciente['ds_nome'], 'id_hospital' => $paciente['id_hospital'], 'dt_nascimento' => $paciente['dt_nascimento'], 'nu_telefone' => $paciente['nu_telefone'], 'nu_atendimento' => $paciente['nu_atendimento']];        
                             }
                         }else{
                             $_SESSION['paciente_redirect'] = ['sistema' => null,'id_paciente' => null, 'buscar' => null];
