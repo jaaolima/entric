4faebbbb7a5fc16825615270bf0c19dcc8ae3c38
João Victor Lima <victorespucoc@gmail.com>
2025-07-07 14:21:31 -0300

    cadastro ambulatorio neonatal

--- sistema/app/view/prescritor_relatorioalta/js.php
+++ sistema/app/view/prescritor_relatorioalta/js.php
@@ -2083,10 +2083,8 @@ $(function(){
             if($(this).hasClass("select2_formula")){
                 div_select = $(this).closest(".div_nova_dieta");
                 valorVolume = $(div_select).find("input[name^='dieta_volume']").val();
-                console.log("Volume 1: "+valorVolume);
                 valorSelect = $(this).val();
                 if (valorSelect && valorSelect.trim() !== '' && valorVolume && valorVolume.trim() !== '') {
-                    console.log("Volume 2: "+valorVolume);
                     $.ajax({
                         type: "POST",
                         url: "ajax/produto_abrir",
@@ -2094,7 +2092,9 @@ $(function(){
                         cache: false,
                         dataType: 'json',
                         success: function( data ){
+                            console.log("div: "+div_select);
                             valorVolume = $(div_select).find("input[name^='dieta_volume']").val();
+                            console.log("Volume 2: "+valorVolume);
                             var kcalProduto = data.kcal ? parseFloat(String(data.kcal).replace(',', '.')) : 0;
                             var ptnProduto = data.ptn ? parseFloat(String(data.ptn).replace(',', '.')) : 0;
                             var fibrasProduto = data.fibras ? parseFloat(String(data.fibras).replace(',', '.')) : 0;
