71a37c873529679735e4db9ef0080f4798d0216e
João Victor Lima <victorespucoc@gmail.com>
2025-08-05 09:51:09 -0300

    hotmail to gmail

--- sistema/app/view/prescritor_prescricaosimplificada/js.php
+++ sistema/app/view/prescritor_prescricaosimplificada/js.php
@@ -1534,7 +1534,7 @@ $(function(){
     $('#agua_kg').mask("9?9");
     $('#agua_dia').mask("9?999");
     $('#telefone').mask("(99)99999-9999");
-    $('#peso').mask('9?99,99');
+    $('#peso').mask('?99,99');
 
     $('#up_kcal_kg').mask("9?9");
     $('#up_kcal_dia').mask("9?999");
