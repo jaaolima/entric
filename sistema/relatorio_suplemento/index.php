a510dd5eb778d1900552d4b2375096abdfe0c739
João Victor Lima <victorespucoc@gmail.com>
2025-08-22 10:19:15 -0300

    marca dagua relatorio

--- sistema/relatorio_suplemento/index.php
+++ sistema/relatorio_suplemento/index.php
@@ -271,7 +271,7 @@ if (trim($relatorio['preparo'])=="") $relatorio['preparo'] = $config['preparo'];
 			<img class="background" style="position:absolute;left:2cm;width:150px;" src="imagem/logo.png" alt="">
 			<img class="background" style="position:absolute;bottom:1cm;right:2px;" src="imagem/efeito.png" alt="">
 			<?php endif; ?>
-			<?php if(!isset($_REQUEST["imprimir"])) : ?>
+			<?php var_dump($_REQUEST['imprimir']); if(!isset($_REQUEST["imprimir"])) : ?>
 			<div class="marca-dagua">NÃO CONCLUÍDA</div>
 			<?php endif; ?>
 			<p class="text-center <?php if($usuario['login'] != 'ibranutro') : ?>linha<?php endif; ?> titulo" style="margin-top:30px;font-size:14px;">PRESCRIÇÃO NUTRICIONAL</p>
