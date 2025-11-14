9ee62c5c772e68dd37b2893e9fb5b3eb825dd308
João Victor Lima <victorespucoc@gmail.com>
2025-11-14 11:19:59 -0300

    erro relatorio alta

--- sistema/api/src/routes/api.php
+++ sistema/api/src/routes/api.php
@@ -3026,9 +3026,6 @@ $app->group("", function () use ($app) {
 
 												$nf_dias_grama = ($nf_grama * $fracionamento_dia);
 
-												var_dump($nf_dias_grama);
-												var_dump($produtos[$i]['kcal']);
-
 												$nf_kcal_dia = ($nf_dias_grama * $produtos[$i]['kcal']) / 100;
 												$nf_kcal_dia = numberFormatPrecision($nf_kcal_dia, 0);
 
