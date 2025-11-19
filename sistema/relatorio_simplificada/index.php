6e67627b19332bbf2856f57a300e388dac6101b8
João Victor Lima <victorespucoc@gmail.com>
2025-11-19 15:07:06 -0300

    problema peso relatorio

--- sistema/relatorio_simplificada/index.php
+++ sistema/relatorio_simplificada/index.php
@@ -973,7 +973,7 @@ if (trim($relatorio['preparo'])=="") $relatorio['preparo'] = $config['preparo'];
 								<td style="width:  100%; border-left: 0px solid #8fcfe5; text-align: center;display:flex;font-size:11px;padding-top:10px;display:flex;flex-wrap:wrap;justify-content:space-around;">
 									<?php 
 									// $danone = $db->select_to_array("distribuidores", "*", "WHERE principal_regiao=0 AND UPPER(uf)='".strtoupper($relatorio['distribuidores'])."'", null);
-									if($paciente['hospital'] == "HOSPITAL SANTA LUCIA SUL" || $paciente['hospital'] == "HOSPITAL SANTA LUCIA NORTE"){
+									if($paciente['hospital'] == "HOSPITAL SANTA LUCIA SUL" || $paciente['hospital'] == "HOSPITAL SANTA LÚCIA NORTE"){
 										$danone = $db->select_to_array("distribuidores", "*", "WHERE id = 24", null);
 										if ($danone){
 											//echo "<p><strong>OUTROS</strong></p>";
