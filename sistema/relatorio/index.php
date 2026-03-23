3c7ad2951317b2114524d4238542b823936df1de
João Victor Lima <victorespucoc@gmail.com>
2026-03-23 14:08:02 -0300

    Alterações validao prescricao manual

--- sistema/relatorio/index.php
+++ sistema/relatorio/index.php
@@ -1107,6 +1107,7 @@ if (trim($relatorio['preparo'])=="") $relatorio['preparo'] = $config['preparo'];
 									$suplementoQuantidade = $suplemento_quantidade->$key;
 									$volumeProduto = $dieta_volume->$key;
 									$HorasCorrer = $hora_correr->$key;
+									var_dump($HorasCorrer);
 									$diluicao = $suplemento_diluicao->$key;
 									$StringSuplementoHorario = '';
 
