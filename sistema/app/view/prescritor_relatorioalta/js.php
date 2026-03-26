9cbed87f556886a56009ae72c8bcc1e3da44b2eb
João Victor Lima <victorespucoc@gmail.com>
2026-03-26 15:08:08 -0300

    erro ao alterar valor peso

--- sistema/app/view/prescritor_relatorioalta/js.php
+++ sistema/app/view/prescritor_relatorioalta/js.php
@@ -3022,7 +3022,7 @@
                 error_alert = 'Por favor, informe o valor do peso em calorias e proteínas na aba "Necessidades Nutricionais".';
             }
 
-            if ($("input[name='calculo_apres_fechado']:checked").val() == 'Fechado') {
+            if ($("input[name='calculo_apres_fechado']:checked").val() == 'Fechado' && $("input[name='tipo_produto']:checked").val() == "Enteral") {
                 if ($("#hidratacao_dia").val() != "") {
                 }
                 else if (error_alert == "") {
@@ -3030,7 +3030,7 @@
                 }
             }
 
-            if (($("input[name='calculo_apres_aberto_liquido']:checked").val() == 'Aberto (Líquido)') || ($("input[name='calculo_apres_aberto_po']:checked").val() == 'Aberto (Pó)')) {
+            if (($("input[name='calculo_apres_aberto_liquido']:checked").val() == 'Aberto (Líquido)') || ($("input[name='calculo_apres_aberto_po']:checked").val() == 'Aberto (Pó)') && $("input[name='tipo_produto']:checked").val() == "Enteral") {
                 if ($("#fracionamento_dia").val() != "") {
                 }
                 else if (error_alert == "") {
